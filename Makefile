bbdef0c9decfe706c9ddfdc08513fd3e2b5be8c8
Hugo Arganda <harganda@luxoft.com>
2023-01-13 21:02:17 -0600

    Create LaTex template

--- Makefile
+++ Makefile
@@ -1,32 +1,20 @@
 #
 # Makefile for Latex resume 
 #
-DEBUG='false'
-
-FILE_NAME = Hugo_Arganda_Resume
 
 # Folders
-SRC_DIR = latex/src
 BUILD_DIR = .build
 
 # XeLaTex options
 CC = xelatex
 OPTS = -output-directory=$(BUILD_DIR)
 
-# Resume folders
-SRC = $(shell find $(SRC_DIR) -name '*.tex')
-
 .PHONY: clean resume.pdf
 
 resume.pdf:
-	@echo "Compile Latex ..."
 	[-d $(BUILD_DIR)] || mkdir -p $(BUILD_DIR)
-ifeq ($(DEBUG) , 'true')
-	@echo $(SRC_DIR)
-	@echo $(SRC)
-endif
-	$(CC) $(OPTS) latex/resume.tex $(SRC)
-	$(CC) $(OPTS) latex/resume.tex $(SRC)
+	$(CC) $(OPTS) resume.tex
+	$(CC) $(OPTS) resume.tex
 
 clean:
 	rm -rf $(BUILD_DIR)/*.pdf
--- render.py
+++ render.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
+import jinja2
 from jinja2 import Template
 import json
-
+import os
 
 input_data = dict()
 
+latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
 
 def load_config():
     global input_data
@@ -14,18 +28,24 @@ def load_config():
 
 def render_index():
     with open('./template/index.html', "r") as f:
-        tmpl = Template(f.read()).render(
-            data=input_data
-        )
+        tmpl = Template(f.read()).render(data=input_data)
         f.close()
         with open("index.html", "w+") as fh:
             fh.write(tmpl)
             fh.close()
 
 
+def render_latex():
+    tmpl = latex_jinja_env.get_template('./template/resume.tex')
+    with open("resume.tex", "w+") as fh:
+        fh.write(tmpl.render(data=input_data))
+        fh.close()
+
+
 def main():
     load_config()
     render_index()
+    render_latex()
 
 
 if __name__ == "__main__":
